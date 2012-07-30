4ef6c272943544c1aa1d369fc909906d91073114
RoyChen <RoyChen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-30 06:07:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/comm100-live-chat/trunk@578950 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin_files/Comm100LiveChatAdmin.class.php
+++ plugin_files/Comm100LiveChatAdmin.class.php
@@ -355,6 +355,26 @@ final class Comm100LiveChatAdmin extends Comm100LiveChat
 		echo <<<HTML
             <script>
                 setTimeout(function() {
+                    window.onresize = function(event) {
+                        var winW = 630, winH = 460;
+                        if (document.body && document.body.offsetWidth) {
+                         winW = document.body.offsetWidth;
+                         winH = document.body.offsetHeight;
+                        }
+                        if (document.compatMode=='CSS1Compat' &&
+                            document.documentElement &&
+                            document.documentElement.offsetWidth ) {
+                         winW = document.documentElement.offsetWidth;
+                         winH = document.documentElement.offsetHeight;
+                        }
+                        if (window.innerWidth && window.innerHeight) {
+                         winW = window.innerWidth;
+                         winH = window.innerHeight;
+                        }
+                        var iframe = document.getElementById('control_panel');
+                        iframe.height = winH-100;
+                    }
+                    window.onresize();
                     if (document.getElementsByClassName == null) {
                         document.getElementsByClassName = function(className){
                            var itemsfound = new Array;
@@ -398,6 +418,26 @@ HTML;
 		echo <<<HTML
             <script>
                 setTimeout(function() {
+                    window.onresize = function(event) {
+                        var winW = 630, winH = 460;
+                        if (document.body && document.body.offsetWidth) {
+                         winW = document.body.offsetWidth;
+                         winH = document.body.offsetHeight;
+                        }
+                        if (document.compatMode=='CSS1Compat' &&
+                            document.documentElement &&
+                            document.documentElement.offsetWidth ) {
+                         winW = document.documentElement.offsetWidth;
+                         winH = document.documentElement.offsetHeight;
+                        }
+                        if (window.innerWidth && window.innerHeight) {
+                         winW = window.innerWidth;
+                         winH = window.innerHeight;
+                        }
+                        var iframe = document.getElementById('control_panel');
+                        iframe.height = winH-100;
+                    }
+                    window.onresize();
                     if (document.getElementsByClassName == null) {
                         document.getElementsByClassName = function(className){
                            var itemsfound = new Array;
